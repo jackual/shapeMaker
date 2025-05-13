beca10c28d8deb66efe33295dcb0005861656594
jackual <162829405+jackual@users.noreply.github.com>
2025-05-13 02:18:20 +0100

    added clusters

--- shapeMaker/ImmersiveView.swift
+++ shapeMaker/ImmersiveView.swift
@@ -12,17 +12,17 @@ import RealityKitContent
 struct sphereMaker {
     public let anchor = AnchorEntity()
     
-    init() {
+    init(_ position: SIMD3<Float>) {
         let sphere = ModelEntity(
-            mesh: .generateSphere(radius: 0.1),
+            mesh: .generateSphere(radius: 0.03),
             materials: [SimpleMaterial(
                 color: .systemBlue,
-                roughness: 0.1,
+                roughness: 0.2,
                 isMetallic: true
                 
             )]
         )
-        anchor.position = SIMD3(x: 0, y: 2, z: -1)
+        anchor.position = position
         anchor.addChild(sphere)
     }
     
@@ -36,16 +36,32 @@ struct sphereMaker {
     }
 }
 
+struct Cluster {
+    let spheres: [sphereMaker]
+    
+    init() {
+        spheres = (0..<5).map { index in
+            let yOffset = Float(index) * 0.1
+            let offset: ClosedRange<Float> = -0.05...0.05
+            let xOffset = Float.random(in: offset)
+            let zOffset = Float.random(in: offset)
+            return sphereMaker(SIMD3(x: xOffset, y: 1.7 + yOffset, z: -1 + zOffset))
+        }
+    }
+}
+
 struct ImmersiveView: View {
     @Environment(AppModel.self) var appModel
     
     var body: some View {
         RealityView { content in
-            let sphere = sphereMaker()
-            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { _ in
-                sphere.move(SIMD3<Float>(x: 0, y: 2, z: -1.2))
+            let cluster = Cluster()
+            //            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { _ in
+            //                sphere.move(SIMD3<Float>(x: 0, y: 2, z: -1.2))
+            //            }
+            for sphere in cluster.spheres {
+                content.add(sphere.anchor)
             }
-            content.add(sphere.anchor)
         }
     }
 }
@@ -54,3 +70,4 @@ struct ImmersiveView: View {
     ImmersiveView()
         .environment(AppModel())
 }
+
