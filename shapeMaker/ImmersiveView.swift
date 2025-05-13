dd883173964911ad02ad136f3768415aadbf11b7
jackual <162829405+jackual@users.noreply.github.com>
2025-05-13 01:55:38 +0100

    sphere struct

--- shapeMaker/ImmersiveView.swift
+++ shapeMaker/ImmersiveView.swift
@@ -9,34 +9,43 @@ import SwiftUI
 import RealityKit
 import RealityKitContent
 
+struct sphereMaker {
+    public let anchor = AnchorEntity()
+    
+    init() {
+        let sphere = ModelEntity(
+            mesh: .generateSphere(radius: 0.1),
+            materials: [SimpleMaterial(
+                color: .systemBlue,
+                roughness: 0.1,
+                isMetallic: true
+                
+            )]
+        )
+        anchor.position = SIMD3(x: 0, y: 2, z: -1)
+        anchor.addChild(sphere)
+    }
+    
+    func move(_ translation: SIMD3<Float>) {
+        let targetTransform = Transform(
+            scale: SIMD3<Float>(x: 1, y: 1, z: 1),
+            rotation: simd_quaternion(0, 0, 0, 1),
+            translation: translation
+        )
+        anchor.move(to: targetTransform, relativeTo: nil, duration: 0.2)
+    }
+}
+
 struct ImmersiveView: View {
     @Environment(AppModel.self) var appModel
-
+    
     var body: some View {
         RealityView { content in
-//            let ambientLight = Entity()
-//            ambientLight.components[PointLightComponent.self] = PointLightComponent(
-//                color: .white,
-//                intensity: 1000,
-//                attenuationRadius: 4.0
-//            )
-//            ambientLight.position = SIMD3(x: 0, y: 2, z: -1)
-            
-            // Create a sphere with material that's more visible
-            let sphere = ModelEntity(
-                mesh: .generateSphere(radius: 0.1),
-                materials: [SimpleMaterial(
-                    color: .systemBlue,
-                    roughness: 0.1,
-                    isMetallic: true
-                )]
-            )
-                        
-            let anchor = AnchorEntity()
-            anchor.position = SIMD3(x: 0, y: 2, z: -1)
-            anchor.addChild(sphere)
-            //anchor.addChild(ambientLight)
-            content.add(anchor)
+            let sphere = sphereMaker()
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { _ in
+                sphere.move(SIMD3<Float>(x: 0, y: 2, z: -1.2))
+            }
+            content.add(sphere.anchor)
         }
     }
 }
