e9bf65daa278a99fddda0716ea5514b9b928d098
jackual <162829405+jackual@users.noreply.github.com>
2025-05-13 15:36:07 +0100

    updated Y axis with avp testing

--- shapeMaker/SpatialStructs.swift
+++ shapeMaker/SpatialStructs.swift
@@ -16,8 +16,8 @@ struct SpatialNote {
     let yPosition: Float
     init (_ relNoteNumber: Float) {
         let normalised = relNoteNumber.normalise(0...27)
-        let low: Float = 1.15
-        let high: Float = 2.15
+        let low: Float = 0.7
+        let high: Float = 1.8
         self.yPosition = low + normalised * (high - low)
     }
 }
