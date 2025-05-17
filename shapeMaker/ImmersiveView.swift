4db00a071b013d7aaae0a5d675f88756e302c66c
jackual <162829405+jackual@users.noreply.github.com>
2025-05-17 19:22:58 +0100

    gitignore and async

--- shapeMaker/ImmersiveView.swift
+++ shapeMaker/ImmersiveView.swift
@@ -33,48 +33,23 @@ extension Pattern {
     }
 }
 
-
-
 struct ImmersiveView: View {
     @Environment(AppModel.self) var appModel
     @State private var clusters: [Cluster] = []
     
     var body: some View {
         RealityView { content in
-            let spatialChord1 = samplePatch.pattern.toSpatialChords()[0]
-            //            let spatialChord2 = SpatialChord([3, 6, 9, 15, 21])
-            //            let spatialChord3 = SpatialChord([2, 4, 11, 16, 23])
-            
-            var cluster1 = Cluster(
+            let spatialChord1 = samplePatch.pattern.toSpatialChords()[7]
+            var cluster1 = await Cluster(
                 x: 0.0,
                 z: -0.4,
                 spatialChord: spatialChord1,
                 content: content,
                 name: "a"
             )
-            //
-            //            var cluster2 = Cluster(
-            //                x: 0.3,
-            //                z: -0.6,
-            //                spatialChord: spatialChord2,
-            //                content: content,
-            //                name: "b"
-            //            )
-            //
-            //            var cluster3 = Cluster(
-            //                x: -0.5,
-            //                z: -1.0,
-            //                spatialChord: spatialChord3,
-            //                content: content,
-            //                name: "cs"
-            //            )
-            //
-            clusters.append(contentsOf: [cluster1/*, cluster2, cluster3*/])
+            clusters.append(contentsOf: [cluster1])
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                //                cluster1.updateChord(SpatialChord([1, 8, 10, 18, 5]))
-                //                cluster2.updateChord(SpatialChord([0, 5, 13, 17, 25]))
-                //                cluster3.updateChord(SpatialChord([4, 6, 12, 20, 22]))
             }
         } update: { content in
             // Update hover states for all clusters
